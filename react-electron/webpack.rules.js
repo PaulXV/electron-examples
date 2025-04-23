4fd98e3754eb48dfe189755e618406544dc9555d
pbrugnola <paolobrugnola@gmail.com>
2025-04-23 15:19:37 +0200

    modified react example

--- react-electron/webpack.rules.js
+++ react-electron/webpack.rules.js
@@ -16,16 +16,6 @@ module.exports = [
       },
     },
   },
-  {
-    test: /\.jsx?$/,
-    use: {
-      loader: 'babel-loader',
-      options: {
-        exclude: /node_modules/,
-        presets: ['@babel/preset-react']
-      }
-    }
-  }
   // Put your webpack loader rules in this array.  This is where you would put
   // your ts-loader configuration for instance:
   /**
@@ -42,4 +32,14 @@ module.exports = [
    *   }]
    * }
    */
+  {
+    test: /\.jsx?$/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        exclude: /node_modules/,
+        presets: ['@babel/preset-react']
+      }
+    }
+  },
 ];
